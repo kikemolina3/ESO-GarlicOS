b63cf409561bc1e98a6b944306cd38061f2662ef
Arey Ferrero <arey.ferrero@estudiants.urv.cat>
2019-11-22 19:57:33 +0100

    progP: Primera versión de la rutina encargada de salvar proceso

--- GARLIC_OS/source/garlic_itcm_proc.s
+++ GARLIC_OS/source/garlic_itcm_proc.s
@@ -103,8 +103,51 @@ _gp_rsiVBL:
 	@; R5: nuevo número de procesos en READY (+1)
 _gp_salvarProc:
 	push {r8-r11, lr}
-
-
+	add r5, #1					@; Se incrementa el número de procesos en la cola de Ready.
+	str r5, [r4]				@; Se almacena el nuevo valor del número de procesos de la cola de Ready en la posición de memoria correspondiente.
+	ldr r8, =_gd_qReady			@; Se carga la dirección de memoria de la cola de Ready.
+	ldrb r9, [r6]				@; Se carga el contenido de la variable _gd_pidz.
+	and r9, #0xF				@; Se filtran los 28 bits correspondientes al identificador de proceso.
+	strb r9, [r8, #15]			@; Se almacena el zócalo del proceso a desbancar en la última posición de la cola de Ready
+	ldr r8, [r13, #60]			@; Se carga el valor del PC del proceso antes de ser desbancado. Se encuentra en la posición 60 de la pila del modo IRQ.
+	mov r10, #24				@; Cada PCB contien 6 campos de 4 bytes cada uno. En consecuencia, el zócalo se deberá multimplicar por 24 para acceder al PCB correspondiente al proceso desbancado.							 					
+	mov r11, #4					@; El campo en donde se almacena el PC es el segundo campo. Se tendrá que sumar 4 para acceder a dicho campo.
+	mla r10, r9, r10, r11		@; Se obtiene la posición de la variable para almacenar el PC del PCB del proceso a desbancar en el vector de PCBs. Se multiplica el zócalo por 24 y se le suma 4.
+	ldr r9, =_gd_pcbs			@; Se carga la dirección de memoria base del vector de PCBs.
+	str r8, [r9, r10]			@; Se almacena el valor del PC del proceso antes de ser desbancado en la posición correspondiente del PCB correspondiente.
+	mrs r8, SPSR				@; Se traslada el contenido del registro SPSR (el CPSR del proceso que se quiere desbancar) a uno de los registros de trabajo.					
+	add r10, #8					@; El campo en donde se almacena la palabra de estado es el quarto campo del PCB. Por tanto, el índice se deberá incrementar 8 unidades.
+	str r8, [r9, r10]			@; Se almacena el contenido del registro CPSR del proceso antes de ser desbancado en la posición correspondiente del PCB correspondiente.
+	mov r8, sp					@; Se almacena el valor del SP (dirección base de la pila) del modo IRQ en un registro de trabajo libre.
+	mrs r9, CPSR				@; Se traslada el contenido del registro CPSR a uno de los registros de trabajo.
+	orr r9, #0x1F				@; Se aplica una máscara para modificar los bits de modo, que pasarán de indicar el modo IRQ al modo System.
+	msr CPSR, r9				@; Se restaura el contenido del registro CPSR. Se entra en modo sistema. Se recupera la pila y el registro de enlace de dicho modo.
+	ldr r10, [r8, #56]			@; Se carga el valor del registro 12 del proceso antes de que este fuese desbancado
+	push {r10, lr}				@; Se apila el contenido del registro 12 y del registro de enlace den la pila del proceso.
+	mov r9, #12					@; R9 será el índice para acceder a la pila del proceso. 
+	mov r11, #0					@; R11 será el contador que permitirá controlar el bucle de apilado de los registros de trabajo.
+.Lbucle_salvarRegistros
+	ldr r10, [r8, r9]			@; Se obtiene el contenido de un registro de trabajo de la pila del modo IRQ...
+	push {r10}					@; ...y se apila en la pila del proceso (pila del modo sistema).
+	sub r9, #4					@; Se incrementa el índice de la pila.
+	add r11, #1					@; Se incrementa el contador de control del bucle.
+							@; Los 12 registros del procesador están agrupados en la pila en 3 grupos de 4 registros por como se han ido almacenando en la ejecución de las distintas rutinas.
+	and r10, r11, #0x3			@; Se filtran los bits altos (se mantienen los 2 bits bajos) del contador de control del bucle.
+	cmp r10, #3					@; Se comprueba si el bucle ha realizado 4 iteraciones, es decir, si se ha almacenado un grupo de 4 regsitros. 
+	blo .Lbucle_salvarRegistros	@; Sino, se continúa ejecutando el bucle.
+	add r9, #32					@; Se incrementa el índice de la pila del proceso para apuntar a otro grupo de 4 registros.
+	cmp r11, #12				@; Se comprueba si se han hecho 12 iteraciones del bucle, es decir, si se han apilado los 12 registros más bajos.
+	blo .Lbucle_salvarRegistros	@; Sino, se continúa ejecutando el bucle.
+	ldr r9, [r6]				@; Se carga el contenido de la variable _gd_pidz.
+	and r9, #0xF				@; Se filtran los 28 bits correspondientes al identificador de proceso.
+	mov r10, #24				@; Cada PCB contien 6 campos de 4 bytes cada uno. En consecuencia, el zócalo se deberá multimplicar por 24 para acceder al PCB correspondiente al proceso desbancado.						 					
+	mov r11, #8					@; El campo en donde se almacena el SP es el tercer campo. Se tendrá que sumar 8 para acceder a dicho campo.
+	mla r10, r9, r10, r11		@; Se obtiene la posición de la variable para almacenar el SP del PCB del proceso a desbancar en el vector de PCBs. Se multiplica el zócalo por 24 y se le suma 8.
+	ldr r9, =_gd_pcbs			@; Se carga la dirección de memoria base del vector de PCBs.
+	str sp, [r9, r10]			@; Se almacena el valor del SP en la posición correspondiente del PCB correspondiente.
+	mrs r11, CPSR				@; Se traslada el contenido del registro CPSR a uno de los registros de trabajo.
+	bic r11, #0x0D				@; Se aplica una máscara para modificar los bits de modo, que pasarán de indicar el modo System al modo IRQ.			
+	msr CPSR, r11				@; Se restaura el contenido del registro CPSR. Se vuelve al modo IRQ.
 	pop {r8-r11, pc}
 
 
@@ -115,8 +158,8 @@ _gp_salvarProc:
 	@; R6: dirección _gd_pidz
 _gp_restaurarProc:
 	push {r8-r11, lr}
-
-
+	
+	
 	pop {r8-r11, pc}
 
 
